a90d2e7afe7b981a5785ba61a879d1f48ca20a2b
Gulzar Ahmed <gulzarhome54@gmail.com>
2018-01-06 23:00:47 +0530

    ticker added

--- index.js
+++ index.js
@@ -97,24 +97,73 @@ const nine =
 [0,0,0,0,4],
 [0,0,0,0,4]]
 
-
-const rowLength = 7;
-const columnLength = 53;
 const boxColor = ["#ebedf0", "#c6e48b", "#7bc96f", "#7bc96f", "196127"];
 
 var x = document.getElementsByClassName("day");
-console.log(x);
+
+var d = new Date(2011,8,22,13,0,0,0);
 
 
-var count = 0;
-for (let i = 0; i<columnLength; i++)
+void drawNumber(no)
 {
-    for (let j = 0; j<rowLength; j++)
+    for (let i = 0; i<5; i++)
     {
-        let c = getColor(artPatterns[j][i])
-        x[count++].style.fill = c;
-    }
+        for (let j = 0; j<5; j++)
+        {
+            let c = getColor(no[j][i])
+            x[count++].style.fill = c;
+        }
+    }  
 }
+
+
+
+function transformMiliseconds(t){
+    //func that transforms miliseconds in digital clock format i.e. 22:34:12
+      var h = Math.floor((t/(1000*60*60))%24);
+      var m = Math.floor((t/(1000*60))%60);
+      var s = Math.floor((t/1000)%60);
+      
+      h = (h <10)?'0'+h:h;
+      m = (m <10)?'0'+m:m;
+      s = (s <10)?'0'+s:s;
+      return s;
+    //   return h+':'+m+':'+s;
+}
+
+//ticker function that will refresh our display every second
+function tick(){
+    var newd = new Date();
+    console.log("Ticker Function Executed");
+    var currentTime = transformMiliseconds(newd-d);
+    console.log("Time x",currentTime);
+    if(currentTime == 0)
+    drawNumber(zero);
+    else if (currentTime == 1)
+    drawNumber(one)
+    else if (currentTime == 2)
+    drawNumber(two)
+    else if (currentTime == 3)
+    drawNumber(three)
+    else if (currentTime == 4)
+    drawNumber(four)
+    else if (currentTime == 5)
+    drawNumber(five)
+    else if (currentTime == 6)
+    drawNumber(six)
+    else if (currentTime == 7)
+    drawNumber(seven)
+    else if (currentTime == 8)
+    drawNumber(eight)
+    else
+    drawNumber(nine)
+
+   }
+
+tick();
+//the runner
+var t = setInterval( tick, 1000);
+
 
 function getColor(v)
 {
